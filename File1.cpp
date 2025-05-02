3f5f383f5d2ded15ba60465a19b2b63ae2ccce0d
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-02 09:28:45 +0700

    Memasukan elemen pada array maksimal 20

--- File1.cpp
+++ File1.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int element[10];
-int panjang;
+int npanjang;
 int x;
+
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 20): ";
+        cin >> npanjang;
+        if (npanjang <= 20) {
+            break;
+        } else {
+            cout << "\nArray maksimal 20 elemen.\n";
+        }
+    }
+
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < npanjang; i++) {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
